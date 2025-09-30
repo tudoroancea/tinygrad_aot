1605dda023d14aa6f5aeec123f48386784dac87f
Tudor Oancea <oancea.tudor@icloud.com>
2025-09-30 15:55:42 +0200

    small benchmark for vmap dimensions

--- test/pol_benchmark.cpp
+++ test/pol_benchmark.cpp
@@ -1,29 +1,31 @@
+// compile the file with the following:
+// clang++ -O3 -mcpu=native -std=c++17 -I$(brew --prefix google-benchmark)/include -L$(brew --prefix google-benchmark)/lib -lbenchmark pol_benchmark.cpp aot_out/pol_1024_2.c aot_out/pol_2_1024.c -o aot_out/pol_benchmark
 #include "benchmark/benchmark.h"
 
-extern void pol_2_10(double *__restrict__ out0_10, double *__restrict__ in0_20);
-extern void pol2_10_2(double *__restrict__ out0_10,
+extern void pol_2_1024(double *__restrict__ out0_1024, double *__restrict__ in0_20);
+extern void pol_1024_2(double *__restrict__ out0_1024,
                       double *__restrict__ in0_20);
 
 #define no_optimize(ptr) asm volatile("" : : "r,m"(ptr[0]) : "memory");
 
-static void pol_2_10(benchmark::State &state) {
-  double out[10], in[20];
+static void bm_pol_2_1024(benchmark::State &state) {
+  double out[1024], in[2048];
 
   for (auto _ : state) {
-    pol_2_10(out, in);
+    pol_2_1024(out, in);
     no_optimize(out);
   }
 }
-BENCHMARK(pol_2_10);
+BENCHMARK(bm_pol_2_1024);
 
-static void pol_10_2(benchmark::State &state) {
-  double out[10], in[20];
+static void bm_pol_1024_2(benchmark::State &state) {
+  double out[1024], in[2048];
 
   for (auto _ : state) {
-    pol2_10_2(out, in);
+    pol_1024_2(out, in);
     no_optimize(out);
   }
 }
-BENCHMARK(pol_10_2);
+BENCHMARK(bm_pol_1024_2);
 
 BENCHMARK_MAIN();
