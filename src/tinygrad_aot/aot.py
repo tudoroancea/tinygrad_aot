755cdbab554f119761afd80648bc497d9588d116
Tudor Oancea <oancea.tudor@icloud.com>
2025-10-01 11:52:01 +0200

    hoist intermediate buffers to workspace buffer

--- src/tinygrad_aot/aot.py
+++ src/tinygrad_aot/aot.py
@@ -21,18 +21,44 @@ def render_kernels(schedule: list[ScheduleItem], var_vals: dict[str, int] | None
 
 @dataclass(frozen=True)
 class BufferClassification:
-  inputs: list[Buffer]
-  outputs: list[Buffer]
-  intermediates: list[Buffer]
+  inputs: tuple[Buffer, ...]
+  outputs: tuple[Buffer, ...]
+  intermediates: tuple[Buffer, ...]
+
+  @cached_property
+  def all_bufs(self):
+    return self.inputs + self.outputs + self.intermediates
 
   @cached_property
   def buf_names(self):
+    """Returns a dict with unique names for each buffer (input/output/intermediate)."""
     input_counter, output_counter, intermediate_counter = count(), count(), count()
     return (
       {buf: f"in{next(input_counter)}_{buf.size}" for buf in self.inputs}
       | {buf: f"out{next(output_counter)}_{buf.size}" for buf in self.outputs}
       | {buf: f"tmp{next(intermediate_counter)}_{buf.size}" for buf in self.intermediates}
     )
+
+  @cached_property
+  def intermediate_buf_offsets(self) -> dict[Buffer, int]:
+    """Returns a dict with the offset in bytes of each intermediate buffer in a global workspace vector.
+    For the moment we assume all the buffers have the same dtype, so that we can ignore alignment causing buffers to not be contiguously laid out.
+    """
+    assert all(buf.dtype == self.all_bufs[0].dtype for buf in self.all_bufs)
+    offsets = {}
+    offset = 0
+    for buf in self.intermediates:
+      from icecream import ic
+
+      ic(buf, offset)
+      offsets[buf] = offset
+      offset += buf.size
+    return offsets
+
+  @cached_property
+  def ws_size(self):
+    """Returns the size in bytes of the global workspace vector."""
+    return sum(buf.size for buf in self.intermediates)
 
 
 def classify_buffers(schedule: list[ScheduleItem], rendered_kernels: list[ProgramSpec]) -> BufferClassification:
@@ -43,9 +69,9 @@ def classify_buffers(schedule: list[ScheduleItem], rendered_kernels: list[Progra
     ctx["outs"].update(set(si.bufs[i] for i in rk.outs))
 
   return BufferClassification(
-    inputs=list(ctx["ins"] - ctx["outs"]),
-    outputs=list(ctx["outs"] - ctx["ins"]),
-    intermediates=list(ctx["ins"] & ctx["outs"]),
+    inputs=tuple(ctx["ins"] - ctx["outs"]),
+    outputs=tuple(ctx["outs"] - ctx["ins"]),
+    intermediates=tuple(ctx["ins"] & ctx["outs"]),
   )
 
 
@@ -59,7 +85,8 @@ def render_schedule(
   c_code = []
   c_code.append('#ifdef __cplusplus\nextern "C" {\n#endif')
   c_code.append("#include <stdlib.h>")
-  c_code.append("#include <stdint.h>")
+  c_code.append("#include <stdint.h>")  # for uint8_t
+  c_code.append("#include <string.h>")  # for memset
 
   # Add kernel sources
   for rk in rendered_kernels:
@@ -72,17 +99,16 @@ def render_schedule(
     + ", ".join(
       [f"{base_renderer.render_dtype(buf.dtype)}* __restrict__ {bc.buf_names[buf]}" for buf in bc.outputs + bc.inputs]
     )
-    + ") {"
+    + ", uint8_t* __restrict__ ws) {"
   )
 
-  # allocate temporary buffers
-  tmpcounter = count(0)
-  tmpnames = {}
-  for tmpbuf in bc.intermediates:
-    assert tmpbuf.size > 0, "Intermediate buffer size must be greater than 0"
-    tmpnames[tmpbuf] = f"tmp{next(tmpcounter)}_{tmpbuf.size}"
-    dtype_str = base_renderer.render_dtype(tmpbuf.dtype)
-    c_code.append(f"\t{dtype_str}* {tmpnames[tmpbuf]} = calloc({tmpbuf.size}, sizeof({dtype_str}));")
+  # clear ws memory
+  c_code.append(f"\tmemset(ws, 0, {bc.ws_size});")
+
+  # extract intermediate buffers from workspace buffer
+  for buf in bc.intermediates:
+    dtype_str = base_renderer.render_dtype(buf.dtype)
+    c_code.append(f"\t{dtype_str}* {bc.buf_names[buf]} = ({dtype_str}*) ws + {bc.intermediate_buf_offsets[buf]};")
 
   # call all kernels
   for si, rk in zip(schedule, rendered_kernels):
@@ -90,10 +116,6 @@ def render_schedule(
     # NOTE: the following doesn't work because in some cases (see bicycle_disc_6 test case) si.bufs
     # contains buffers that are not passed to the kernel. WTF
     # c_code.append(f"\t{rk.function_name}({','.join(bc.buf_names[buf] for buf in si.bufs)}, 0);")
-
-  # free temporary buffers
-  for tmpbuf in bc.intermediates:
-    c_code.append(f"\tfree({tmpnames[tmpbuf]});")
 
   c_code.append("}")
 
@@ -121,5 +143,7 @@ def aot(name: str, fn: Codegenable, inshape: OneOrMore[Shape]):
 
   # Buffer classification
   bc = classify_buffers(schedule, rendered_kernels)
+
+  bc.intermediate_buf_offsets
 
   return render_schedule(name, schedule, rendered_kernels, bc)
