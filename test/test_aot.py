cd97c989f7e23d4767ba2e195858c2a06562ae94
Tudor Oancea <oancea.tudor@icloud.com>
2025-10-01 17:10:59 +0200

    refactor codegened functions

--- test/test_aot.py
+++ test/test_aot.py
@@ -4,7 +4,6 @@ from functools import wraps
 from typing import Callable
 
 import pytest
-from icecream import ic
 from tinygrad import Tensor
 from tinygrad.helpers import prod
 from tinygrad.uop.ops import UOp
@@ -43,19 +42,17 @@ def do_square(x: Tensor) -> Tensor:
 do_square_grad = grad(do_square)
 
 
-def _bicycle_ocp(x: Tensor, u: Tensor) -> Tensor:
-  theta = x[2]
-  v = x[3]
-  a = u[0]
-  delta = u[1]
-  dx = v * Tensor.cos(theta)
-  dy = v * Tensor.sin(theta)
-  dtheta = v * Tensor.tan(delta)
-  dv = a
-  return Tensor.stack(dx, dy, dtheta, dv)
+def euler(fn: Callable[[Tensor, Tensor], Tensor], x: Tensor, u: Tensor, dt: float) -> Tensor:
+  return x + dt * fn(x, u)
 
 
-def _rk4(fn: Callable[[Tensor], Tensor], x: Tensor, u: Tensor, dt: float) -> Tensor:
+def rk2(fn: Callable[[Tensor, Tensor], Tensor], x: Tensor, u: Tensor, dt: float) -> Tensor:
+  k1 = fn(x, u)
+  k2 = fn(x + dt * k1 / 2, u)
+  return x + dt * k2
+
+
+def rk4(fn: Callable[[Tensor, Tensor], Tensor], x: Tensor, u: Tensor, dt: float) -> Tensor:
   k1 = fn(x, u)
   k2 = fn(x + dt * k1 / 2, u)
   k3 = fn(x + dt * k2 / 2, u)
@@ -63,24 +60,22 @@ def _rk4(fn: Callable[[Tensor], Tensor], x: Tensor, u: Tensor, dt: float) -> Ten
   return x + dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 
-def bicycle_cont(z: Tensor) -> Tensor:
-  return _bicycle_ocp(z[:4], z[4:])
-
-
-def bicycle_cont2(z: Tensor) -> Tensor:
-  theta = z[:, 2]
-  v = z[:, 3]
-  a = z[:, 4]
-  delta = z[:, 5]
+def bicycle_cont(x: Tensor, u: Tensor) -> Tensor:
+  theta = x[2]
+  v = x[3]
+  a = u[0]
+  delta = u[1]
   dx = v * Tensor.cos(theta)
   dy = v * Tensor.sin(theta)
-  dtheta = v * Tensor.tan(delta)
+  dtheta = v * Tensor.tan(delta).contiguous()
   dv = a
-  return Tensor.stack([dx, dy, dtheta, dv], dim=0)
+  return Tensor.stack(dx, dy, dtheta, dv)
 
 
-def bicycle_disc(z: Tensor) -> Tensor:
-  return _rk4(_bicycle_ocp, z[:4], z[4:], 0.01)
+def lti_cont(x: Tensor, u: Tensor):
+  # NOTE: the following will incur a copy from a buffer with PYTHON device to a buffer with CPU device (no inlined constant)
+  # return Tensor(np.array([[1.0, 2.0], [0.0, 3.0]])) @ x + Tensor(np.array([[0.0], [1.0]])) @ u
+  return Tensor.stack(x[0] + 2 * x[1], 3 * x[1] + u[0])
 
 
 def pol(x: Tensor) -> Tensor:
@@ -105,7 +100,7 @@ def eq_constraints(Z: Tensor) -> Tensor:
   N = Z.shape[0] // (nx + nu)
   # NOTE: contiguous below makes the same state be contiguous in memory and should help with vectorization
   staging = Z.pad_to((nx + nu) * (N + 1)).reshape((N + 1, nx + nu)).transpose().contiguous()  # shape (nx + nu, N + 1)
-  interstage = staging[:nx, 1:] - _rk4(_bicycle_ocp, staging[:nx, :N], staging[nx:, :N], dt=1.0)  # shape (nx, N)
+  interstage = staging[:nx, 1:] - rk4(bicycle_cont, staging[:nx, :N], staging[nx:, :N], dt=1.0)  # shape (nx, N)
   return Tensor.cat(Z[:nx], interstage.transpose().flatten().contiguous())  # is this contiguous necessary?
 
 
@@ -113,25 +108,33 @@ def eq_constraints(Z: Tensor) -> Tensor:
 # Test AOT
 ##################################################################
 
+
 args = [
   ("do_square_8", do_square, (8,)),
   ("do_square_1024", do_square, (1024,)),
   ("linear_sum", linear_sum, (1024,)),
-  pytest.param("linear_sum_grad", linear_sum_grad, (1024,), marks=pytest.mark.skip),
-  ("bicycle_cont_two_params", _bicycle_ocp, ((4,), (2,))),
-  ("bicycle_cont_6", bicycle_cont, (6,)),
-  ("bicycle_cont_6_10", bicycle_cont, (6, 10)),
-  ("bicycle_cont2_10_6", bicycle_cont2, (10, 6)),
-  ("bicycle_disc_6", bicycle_disc, (6,)),
-  ("bicycle_disc_6_10", bicycle_disc, (6, 10)),
+  ("bicycle_cont", bicycle_cont, ((4,), (2,))),
+  ("bicycle_cont_10", bicycle_cont, ((4, 10), (2, 10))),
+  ("bicycle_rk2", lambda x, u: rk2(bicycle_cont, x, u, 0.01), ((4,), (2,))),
+  ("bicycle_rk4", lambda x, u: rk4(bicycle_cont, x, u, 0.01), ((4,), (2,))),
   ("pol_2", pol, (2,)),
   ("pol_2_1024", pol, (2, 1024)),
   ("pol_1024_2", pol2, (1024, 2)),
   ("eq_constraints_10", eq_constraints, ((4 + 2) * 10 + 4,)),
+  ("lti_cont", lti_cont, ((2,), (1,))),
+  ("lti_cont_10", lti_cont, ((2, 10), (1, 10))),
+  ("lti_euler", lambda x, u: euler(lti_cont, x, u, 0.01), ((2,), (1,))),
+  ("lti_euler_10", lambda x, u: euler(lti_cont, x, u, 0.01), ((2, 10), (1, 10))),
+  ("lti_rk2", lambda x, u: rk2(lti_cont, x, u, 0.01), ((2,), (1,))),
+  ("lti_rk2_10", lambda x, u: rk2(lti_cont, x, u, 0.01), ((2, 10), (1, 10))),
+  ("lti_rk4", lambda x, u: rk4(lti_cont, x, u, 0.01), ((2,), (1,))),
+  ("lti_rk4_10", lambda x, u: rk4(lti_cont, x, u, 0.01), ((2, 10), (1, 10))),
+  # pytest.param("linear_sum_grad", linear_sum_grad, (1024,), marks=pytest.mark.skip),
 ]
+argids = [arg[0] for arg in args]
 
 
-@pytest.mark.parametrize("name, fn, inshape", args)
+@pytest.mark.parametrize("name, fn, inshape", args, ids=argids)
 def test_run(name: str, fn: Codegenable, inshape: OneOrMore[Shape]):
   ins = (
     (Tensor.arange(prod(inshape)).reshape(inshape),)
@@ -141,7 +144,7 @@ def test_run(name: str, fn: Codegenable, inshape: OneOrMore[Shape]):
   print(fn(*ins).numpy())
 
 
-@pytest.mark.parametrize("name, fn, inshape", args)
+@pytest.mark.parametrize("name, fn, inshape", args, ids=argids)
 def test_aot(name: str, fn: Codegenable, inshape: OneOrMore[Shape]):
   prefix = os.path.join(os.path.dirname(__file__), "aot_out")
   os.makedirs(prefix, exist_ok=True)
